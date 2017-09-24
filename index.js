4dad9b253c93c0b4f0a9a6061f9a4782fa1a4c67
Annette Michaels <micha232@umn.edu>
2017-09-24 07:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var height = 70
 module.exports = { name, height
 }
 
-//Code for the first part of the lab but removing it makes it resolve not loop this statement
+//Code for the first part of the lab but removing it makes it require not run this statement
 /*if (name === "Susan") {
   console.log("The name is correct")
 } else {
